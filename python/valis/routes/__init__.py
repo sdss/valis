9eb327a277bcd0dcab3152837b8fc24c11cd2665
Brian Cherinka <havok2063@hotmail.com>
2020-09-17 17:07:56 -0400

    initial setup of fastapi application

---
+++ python/valis/routes/__init__.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Filename: __init__.py
+# Project: routes
+# Author: Brian Cherinka
+# Created: Wednesday, 16th September 2020 6:09:51 pm
+# License: BSD 3-clause "New" or "Revised" License
+# Copyright (c) 2020 Brian Cherinka
+# Last Modified: Wednesday, 16th September 2020 6:09:51 pm
+# Modified By: Brian Cherinka
+
+
+from __future__ import print_function, division, absolute_import
---
+++ python/valis/routes/access.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Filename: access.py
+# Project: routes
+# Author: Brian Cherinka
+# Created: Wednesday, 16th September 2020 6:16:40 pm
+# License: BSD 3-clause "New" or "Revised" License
+# Copyright (c) 2020 Brian Cherinka
+# Last Modified: Wednesday, 16th September 2020 6:16:40 pm
+# Modified By: Brian Cherinka
+
+
+from __future__ import print_function, division, absolute_import
