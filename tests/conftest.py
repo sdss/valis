27547143d40960c3f0da339d69d7872506d8fb51
Brian Cherinka <havok2063@hotmail.com>
2022-07-19 13:30:33 -0400

    adding in test maskbits

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,9 @@ underlying directories. See https://docs.pytest.org/en/2.7.3/plugins.html for
 more information.
 """
 
+import pathlib
+import shutil
+
 import pytest
 from fastapi.testclient import TestClient
 from valis.main import app
@@ -23,3 +26,14 @@ app.dependency_overrides[set_auth] = override_auth
 @pytest.fixture(scope='module')
 def client():
     yield TestClient(app)
+
+
+@pytest.fixture()
+def monkeymask(monkeypatch, tmp_path):
+    svn_dir = tmp_path / 'svn'
+    mask_dir = svn_dir / 'repo/sdss/idlutils/trunk/data/sdss/sdssMaskbits.par'
+    mask_dir.parent.mkdir(parents=True, exist_ok=True)
+    testpath = pathlib.Path(__file__).parent / 'data/sdssMaskbits.par'
+    shutil.copy2(testpath, mask_dir)
+
+    monkeypatch.setenv('SDSS_SVN_ROOT', svn_dir)
---
+++ tests/test_maskbits.py
@@ -0,0 +1,57 @@
+# encoding: utf-8
+#
+import pytest
+
+pytest.mark.usefixtures('monkeymask')
+
+def get_data(response):
+    assert response.status_code == 200
+    return response.json()
+
+def test_maskbits_list(client):
+    response = client.get("/maskbits/list")
+    data = get_data(response)
+    assert 'MANGA_DAPQUAL' in data['schema']
+    assert 'APOGEE2_TARGET3' in data['schema']
+
+def test_maskbits_schema(client):
+    response = client.get("/maskbits/schema?schema=MANGA_DRP2QUAL")
+    data = get_data(response)
+    data = data['MANGA_DRP2QUAL']
+    assert 'bit' in data
+    assert 'RAMPAGINGBUNNY' in data['label']
+    assert " Rampaging dust bunnies in IFU flats" in data['description']
+
+def test_maskbits_bit_to_value(client):
+    response = client.get("/maskbits/bits/value?bits=2&bits=8")
+    data = get_data(response)
+    assert data['value'] == 260
+
+def test_maskbits_bit_to_labels(client):
+    response = client.get("/maskbits/bits/labels?bits=2&bits=8&schema=MANGA_DRP2QUAL")
+    data = get_data(response)
+    assert data['labels'] == ["EXTRACTBRIGHT", "ARCFOCUS"]
+
+def test_maskbits_labels_to_value(client):
+    response = client.get("/maskbits/labels/value?labels=BADIFU&labels=SCATFAIL&schema=MANGA_DRP2QUAL")
+    data = get_data(response)
+    assert data['value'] == 80
+
+def test_maskbits_labels_to_bits(client):
+    response = client.get("/maskbits/labels/bits?labels=BADIFU&labels=SCATFAIL&schema=MANGA_DRP2QUAL")
+    data = get_data(response)
+    assert data['bits'] == [4, 6]
+
+def test_maskbits_value_to_bits(client):
+    response = client.get("/maskbits/value/bits?value=260&schema=MANGA_DRP2QUAL")
+    data = get_data(response)
+    assert data['bits'] == [2, 8]
+
+def test_maskbits_value_to_labels(client):
+    response = client.get("/maskbits/value/labels?value=260&schema=MANGA_DRP2QUAL")
+    data = get_data(response)
+    assert data['labels'] == ["EXTRACTBRIGHT", "ARCFOCUS"]
+
+
+
+
