9935a3527a36989f4d3885fe00d8d95aacb50c4e
havok2063 <havok2063@hotmail.com>
2026-07-23 15:46:31 -0400

    fixing test

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,7 +8,7 @@ from pytest import mark
 def test_main(client):
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Hello SDSS": "This is the FastAPI World", 'release': "DR19"}
+    assert response.json() == {"Hello SDSS": "This is the FastAPI World", 'release': "DR20"}
 
 @mark.parametrize('release', ['WORK', 'DR17'])
 def test_main_with_release(client, release):
